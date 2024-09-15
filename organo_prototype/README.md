501b760ba48f10ac1822a0e035dd9c0625b76585
Bruno-Cunha-Souza <brunosouza.cup@gmail.com>
2024-09-15 15:19:50 -0300

    finale ola-mundo

--- organo_prototype/README.md
+++
@@ -1,3 +0,0 @@
-# Design
-
-https://www.figma.com/community/file/1410393986549302425/react-desenvolvendo-com-javascript-organo
--- organo_prototype/src/components/Button/index.js
+++
@@ -1,6 +0,0 @@
-import "./Button.css";
-
-const Button = (props) => {
-  return <button>{props.children}</button>;
-};
-export default Button;
